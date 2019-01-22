5e294234f7d82585a3dd10719ae9c5b6b581c9f6
André Langlais <derkan.man@gmail.com>
2019-01-22 16:21:16 -0500

    Update ch.c

--- ch.c
+++ ch.c
@@ -7,13 +7,21 @@ problèmes connus:
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 int main (void)
 {
   fprintf (stdout, "%% ");
 
   /* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter. */
-
+  int bytes_read;
+  size_t size;
+  char *input = NULL;
+  while (bytes_read = getline(&input, &size, stdin)) {
+	  fprintf(stdout, "%% ");
+	  //fork() et exec() avec le bon programme
+  }
+  free(input);
   fprintf (stdout, "Bye!\n");
   exit (0);
 }
