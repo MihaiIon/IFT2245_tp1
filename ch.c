aef9877283c1b04485d0e3cc078ed4fae3546752
André Langlais <derkan.man@gmail.com>
2019-01-27 12:02:43 -0500

    fork() and exec() first test

--- ch.c
+++ ch.c
@@ -8,18 +8,40 @@ problèmes connus:
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <unistd.h> 
+#include<sys/wait.h> 
+#include<string.h> 
+
 
 int main (void)
 {
   fprintf (stdout, "%% ");
-
   /* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter. */
   int bytes_read;
   size_t size;
   char *input = NULL;
-  while (bytes_read = getline(&input, &size, stdin)) {
-	  fprintf(stdout, "%% ");
-	  //fork() et exec() avec le bon programme
+  pid_t pid;
+
+  //setvbuf(stdout, NULL, _IONBF, 0);
+  while(bytes_read = getline(&input, &size, stdin)){
+    fprintf (stdout, "%% ");
+    //parse the string
+
+    pid = fork();
+    if(pid < 0){
+      return 1;
+    }
+    else if(pid == 0) {
+      //handling child process
+      execlp("ls","ls", NULL); 
+      exit(0);
+    }
+    else{
+      //parent process
+      pid_t pid_return_val;
+      int status;
+      pid_return_val = wait(&status); //do something with return val
+    }
   }
   free(input);
   fprintf (stdout, "Bye!\n");
